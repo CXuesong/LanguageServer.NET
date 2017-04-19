3b3cf41fa623f6a546bedc5eb5533f54e0c95122
forest93 <forest93@163.com>
2017-04-19 13:49:14 +0800

    Add DemoLanguageServer project.

---
+++ DemoLanguageServer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using LanguageServer.VsCode.JsonRpc;
+using LanguageServer.VsCode.Server;
+using LangServer = LanguageServer.VsCode.Server.LanguageServer;
+
+namespace DemoLanguageServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var cin = Console.OpenStandardInput())
+            using (var cout = Console.OpenStandardOutput())
+            {
+                var connection = Connection.FromStreams(cin, cout, new MyStreamMessageLogger());
+                using (var server = new LangServer(connection))
+                {
+                    server.Start();
+                }
+            }
+        }
+    }
+
+    class MyStreamMessageLogger : IStreamMessageLogger
+    {
+        /// <inheritdoc />
+        public void NotifyMessageSent(string content)
+        {
+            var message = "< " + content;
+            Trace.WriteLine(message);
+        }
+
+        /// <inheritdoc />
+        public void NotifyMessageReceived(string content)
+        {
+            var message = "> " + content;
+            Trace.WriteLine(message);
+        }
+    }
+}
