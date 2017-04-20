55462e0691001d24ec65cbece5536936c25b8617
forest93 <forest93@163.com>
2017-04-20 11:27:23 +0800

    Handles `initialized` in LanguageServer.

--- LanguageServer.VsCode/Server/LanguageServer.General.cs
+++ LanguageServer.VsCode/Server/LanguageServer.General.cs
@@ -16,8 +16,10 @@ namespace LanguageServer.VsCode.Server
                     return new {Capabilities = e.ServerCapabilities};
                 }
             ));
-            requestHandlerDict.Add("shutdown", CreateHandler<GeneralRequestEventArgs>(e => OnShutdown()));
-            requestHandlerDict.Add("exit", CreateHandler<GeneralRequestEventArgs>(e => OnExit()));
+            requestHandlerDict.Add("initialized", CreateHandler<GeneralRequestEventArgs>(e => OnInitialized(e)));
+            requestHandlerDict.Add("shutdown", CreateHandler<GeneralRequestEventArgs>(e => OnShutdown(e)));
+            requestHandlerDict.Add("exit", CreateHandler<GeneralRequestEventArgs>(e => OnExit(e)));
+
         }
 
         /// <summary>
@@ -35,21 +37,37 @@ namespace LanguageServer.VsCode.Server
         }
 
         /// <summary>
+        /// The initialized notification is sent from the client to the server after the client
+        /// received the result of the initialize request but before the client is sending
+        /// any other request or notification to the server. The server can use the initialized
+        /// notification for example to dynamically register capabilities.
+        /// </summary>
+        public event EventHandler<GeneralRequestEventArgs> Initialized;
+
+        /// <summary>
+        /// Raises the <see cref="Initialized"/> event.
+        /// </summary>
+        protected virtual void OnInitialized(GeneralRequestEventArgs e)
+        {
+            Initialized?.Invoke(this, e);
+        }
+
+        /// <summary>
         /// The shutdown request is sent from the client to the server.
         /// It asks the server to shut down, but to not exit
         /// (otherwise the response might not be delivered correctly to the client).
         /// There is a separate exit notification that asks the server to exit.
         /// </summary>
-        public event EventHandler Shutdown;
+        public event EventHandler<GeneralRequestEventArgs> Shutdown;
 
         /// <summary>
         /// Raises the <see cref="Shutdown"/> event.
         /// </summary>
-        protected virtual void OnShutdown()
+        protected virtual void OnShutdown(GeneralRequestEventArgs e)
         {
             try
             {
-                Shutdown?.Invoke(this, EventArgs.Empty);
+                Shutdown?.Invoke(this, e);
             }
             finally
             {
@@ -62,14 +80,14 @@ namespace LanguageServer.VsCode.Server
         /// </summary>
         /// <remarks>The server should exit with success code 0 if the shutdown request has been received before;
         /// otherwise with error code 1.</remarks>
-        public event EventHandler Exit;
+        public event EventHandler<GeneralRequestEventArgs> Exit;
 
         /// <summary>
         /// Raises the <see cref="Exit"/> event.
         /// </summary>
-        protected virtual void OnExit()
+        protected virtual void OnExit(GeneralRequestEventArgs e)
         {
-            Exit?.Invoke(this, EventArgs.Empty);
+            Exit?.Invoke(this, e);
         }
     }
 }
