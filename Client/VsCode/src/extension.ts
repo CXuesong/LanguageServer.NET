eb1119ac851c0f06d3079c54a4e0df42d92bef9f
forest93 <forest93@163.com>
2020-03-21 22:07:06 +0800

    Update VSCode client support to vscode 1.33+.

--- Client/VsCode/src/extension.ts
+++ Client/VsCode/src/extension.ts
@@ -9,75 +9,81 @@ import * as fs from "fs";
 
 // Defines the search path of your language server DLL. (.NET Core)
 const languageServerPaths = [
-	"server/DemoLanguageServer.dll",
-	"../../DemoLanguageServer/bin/Debug/netcoreapp2.0/DemoLanguageServer.dll",
+    "server/DemoLanguageServer.dll",
+    "../../DemoLanguageServer/bin/Debug/netcoreapp3.1/DemoLanguageServer.dll",
 ]
 
-function activateLanguageServer(context: vscode.ExtensionContext) {
-	// The server is implemented in an executable application.
-	let serverModule: string = null;
-	for (let p of languageServerPaths) {
-		p = context.asAbsolutePath(p);
-		// console.log(p);
-		if (fs.existsSync(p)) {
-			serverModule = p;
-			break;
-		}
-	}
-	if (!serverModule) throw new URIError("Cannot find the language server module.");
-	let workPath = path.dirname(serverModule);
-	console.log(`Use ${serverModule} as server module.`);
-	console.log(`Work path: ${workPath}.`);
+let client: languageClient.LanguageClient | undefined;
+
+async function activateLanguageServer(context: vscode.ExtensionContext) {
+    // The server is implemented in an executable application.
+    let serverModule: string | undefined;
+    for (let p of languageServerPaths) {
+        p = context.asAbsolutePath(p);
+        console.log(p);
+        try {
+            await fs.promises.access(p);
+            serverModule = p;
+            break;
+        } catch (err) {
+            // Skip this path.
+        }
+    }
+    if (!serverModule) throw new URIError("Cannot find the language server module.");
+    let workPath = path.dirname(serverModule);
+    console.log(`Use ${serverModule} as server module.`);
+    console.log(`Work path: ${workPath}.`);
 
 
-	// If the extension is launched in debug mode then the debug server options are used
-	// Otherwise the run options are used
-	let serverOptions: languageClient.ServerOptions = {
-		run: { command: "dotnet", args: [serverModule], options: { cwd: workPath } },
-		debug: { command: "dotnet", args: [serverModule, "--debug"], options: { cwd: workPath } }
-	}
-	// Options to control the language client
-	let clientOptions: languageClient.LanguageClientOptions = {
-		// Register the server for plain text documents
-		documentSelector: ["demolang"],
-		synchronize: {
-			// Synchronize the setting section 'languageServerExample' to the server
-			configurationSection: "demoLanguageServer",
-			// Notify the server about file changes to '.clientrc files contain in the workspace
-			fileEvents: [
-				vscode.workspace.createFileSystemWatcher("**/.clientrc"),
-				vscode.workspace.createFileSystemWatcher("**/.demo"),
-			]
-		},
-	}
+    // If the extension is launched in debug mode then the debug server options are used
+    // Otherwise the run options are used
+    let serverOptions: languageClient.ServerOptions = {
+        run: { command: "dotnet", args: [serverModule], options: { cwd: workPath } },
+        debug: { command: "dotnet", args: [serverModule, "--debug"], options: { cwd: workPath } }
+    }
+    // Options to control the language client
+    let clientOptions: languageClient.LanguageClientOptions = {
+        // Register the server for plain text documents
+        documentSelector: ["demolang"],
+        synchronize: {
+            // Synchronize the setting section 'languageServerExample' to the server
+            configurationSection: "demoLanguageServer",
+            // Notify the server about file changes to '.clientrc files contain in the workspace
+            fileEvents: [
+                vscode.workspace.createFileSystemWatcher("**/.clientrc"),
+                vscode.workspace.createFileSystemWatcher("**/.demo"),
+            ]
+        },
+    }
 
-	// Create the language client and start the client.
-	let client = new languageClient.LanguageClient("demoLanguageServer", "Demo Language Server", serverOptions, clientOptions);
-	let disposable = client.start();
+    // Create the language client and start the client.
+    client = new languageClient.LanguageClient("demoLanguageServer", "Demo Language Server", serverOptions, clientOptions);
+    let disposable = client.start();
 
-	// Push the disposable to the context's subscriptions so that the 
-	// client can be deactivated on extension deactivation
-	context.subscriptions.push(disposable);
+    // Push the disposable to the context's subscriptions so that the 
+    // client can be deactivated on extension deactivation
+    context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	console.log("demolang extension is now activated.");
+export async function activate(context: vscode.ExtensionContext) {
+    console.log("demolang extension is now activated.");
 
-	activateLanguageServer(context);
+    await activateLanguageServer(context);
 
-	// The command has been defined in the package.json file
-	let disposable = vscode.commands.registerCommand("extension.sayHello", () => {
-		// The code you place here will be executed every time your command is executed
+    // The command has been defined in the package.json file
+    let disposable = vscode.commands.registerCommand("extension.sayHello", () => {
+        // The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage("Hello World!");
-	});
+        // Display a message box to the user
+        vscode.window.showInformationMessage("Hello World!");
+    });
 
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {
+export async function deactivate(): Promise<void> {
+    client?.stop();
 }
