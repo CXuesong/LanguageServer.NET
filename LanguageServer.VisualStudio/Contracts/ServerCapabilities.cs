3510dd480a8c21b06adf764c45a842049840b53c
forest93 <forest93@163.com>
2020-05-23 18:19:41 +0800

    Add LanguageServer.VisualStudio project.

---
+++ LanguageServer.VisualStudio/Contracts/ServerCapabilities.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LanguageServer.VsCode.Contracts;
+using Newtonsoft.Json;
+
+namespace LanguageServer.VisualStudio.Contracts
+{
+    public class VsServerCapabilities : ServerCapabilities
+    {
+
+        /// <summary>
+        /// (Visual Studio) The server provides hover support.
+        /// </summary>
+        /// <remarks>VS does not support object value for now.</remarks>
+        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public new bool HoverProvider
+        {
+            get => base.HoverProvider != null;
+            set => base.HoverProvider = value ? (base.HoverProvider ?? new HoverOptions()) : null;
+        }
+
+    }
+}
